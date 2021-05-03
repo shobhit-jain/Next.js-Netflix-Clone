25cea51aaa0a3c974608ae56363d09849777b731
Moin-A <43196743+Moin-A@users.noreply.github.com>
2021-05-03 09:14:15 +0530

    Landing page (#3)
    * first commit
    
    * Internationalizationa and background image agged
    
    * Internationalizationa and background image agged
    
    Co-authored-by: moinahmed22feet <moin.ahmed@22feettribalww.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,33 @@ import { NextPage } from 'next'
 import React from 'react'
 import Link from 'next/link'
 import { Footer } from '@/components/common/footer'
-import { Head } from '@/components/common/head'
+import Nav from '../components/common/Nav'
+import Image from 'next/image'
 
 export const Landing_Page: NextPage = () => {
   return (
     <>
-      <Head title="Netflix Clone Web – Watch TV Shows Online, Watch Movies Online" />
-
-      <div className="bg-blue h-screen bg-opacity-70 flex flex-col justify-center items-center">
-        <h1 className="text-4xl text-white mb-7 font-serif">
-          Welcome To Landing Page
-        </h1>
-
-        <div>
-          <Link href="/login">
-            <a className="rounded py-2 px-4 bg-blue text-white hover:border">
-              Sign in
-            </a>
-          </Link>
+      <div className="">
+        <div className="h-screen p-4">
+          <Image
+            alt="Hero"
+            src="/netflix_background.jpeg"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+          <Nav />
+          <a
+            href="/login"
+            className="authLinks redButton"
+            data-uia="header-login-link"
+          >
+            Sign In
+          </a>
         </div>
+
+        <Footer />
       </div>
-      <Footer />
     </>
   )
 }
