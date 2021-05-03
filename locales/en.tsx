66c2843272b4df1cef2bf4bf8c617c0f52c32869
Moin-A <moinqa6@gmail.com>
2021-05-03 09:02:34 +0530

    Internationalizationa and background image agged

---
+++ locales/en.tsx
@@ -0,0 +1,17 @@
+export default {
+  subtitle: 'Accelerating science',
+  signin: 'Sign In',
+  directory: 'Directory',
+  hero: 'The birth of the Web',
+  description: `The World Wide Web was invented by British scientist Tim Berners-Lee in 1989 while working at CERN`,
+  cookies: `This website uses cookies to ensure you get the best experience on our website`,
+  button: `Got it!`,
+  about: 'About',
+  news: 'News',
+  science: 'Science',
+  research: 'Research',
+  search: 'Search',
+  p1: `Tim Berners-Lee, a British scientist, invented the World Wide Web (WWW) in 1989, while working at CERN. The web was originally conceived and developed to meet the demand for automated information-sharing between scientists in universities and institutes around the world.`,
+  p2: `The first website at CERN – and in the world – was dedicated to the World Wide Web project itself and was hosted on Berners-Lee's NeXT computer. In 2013, CERN launched a project to restore this first ever website: info.cern.ch.`,
+  p3: `On 30 April 1993, CERN put the World Wide Web software in the public domain. Later, CERN made a release available with an open licence, a more sure way to maximise its dissemination. These actions allowed the web to flourish.`,
+}
