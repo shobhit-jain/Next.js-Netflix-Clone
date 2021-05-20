4ab18108b70a452eca09aa033627d284986cac73
shobhit-jain <shobhitjain.work@gmail.com>
2021-05-21 02:48:49 +0530

    locales (en) for landing-page section

---
+++ locales/page-sections/landing-page/@Section-Enjoy-on-your-tv/en.ts
@@ -0,0 +1,4 @@
+export default {
+  h1: 'Enjoy on your TV.',
+  h2: `Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.`,
+}
---
+++ locales/page-sections/landing-page/@Section-Frequently-asked-questions/en.ts
@@ -0,0 +1,3 @@
+export default {
+  h1: 'Frequently Asked Questions',
+}
