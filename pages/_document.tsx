30ea9c8c565f812c51cc7d6947a23af84dc2aa82
shobhit-jain <shobhitjain.work@gmail.com>
2021-06-07 10:47:34 +0530

    commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,66 @@ class InlineStylesHead extends Head {
   }
 }
 
+const MetaData = () => {
+  return (
+    <>
+      {/* preloading fonts */}
+      <link
+        rel="preload"
+        href={`${domain_prefix}/fonts/Inter/Inter-Regular.ttf`}
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href={`${domain_prefix}/fonts/Inter/Inter-Medium.ttf`}
+        as="font"
+        crossOrigin=""
+      />
+
+      {/* icons */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="favicon/apple-touch-icon.png"
+      />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="favicon/favicon-32x32.png"
+      />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="favicon/favicon-16x16.png"
+      />
+
+      <link
+        rel="mask-icon"
+        href="favicon/safari-pinned-tab.svg"
+        color="#e50914"
+      />
+
+      {/* site manifest */}
+      <link rel="manifest" href="manifest/site.webmanifest" />
+
+      {/* web-app title for chrome */}
+      <meta name="application-name" content="Netflix" />
+
+      {/* web-app title for apple */}
+      <meta name="apple-mobile-web-app-title" content="Netflix" />
+
+      {/* theme color for web-app */}
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#000000" />
+    </>
+  )
+}
+
 class MyDocument extends Document {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   static async getInitialProps(ctx: DocumentContext) {
@@ -45,18 +105,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <InlineStylesHead>
-          <link
-            rel="preload"
-            href={`${domain_prefix}/fonts/Inter/Inter-Regular.ttf`}
-            as="font"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href={`${domain_prefix}/fonts/Inter/Inter-Medium.ttf`}
-            as="font"
-            crossOrigin=""
-          />
+          <MetaData />
         </InlineStylesHead>
 
         <body>
