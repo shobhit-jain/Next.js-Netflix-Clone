1be19059f14d3299f511bc90295ecd4287947748
shobhit-jain <shobhitjain.work@gmail.com>
2021-06-08 17:02:35 +0530

    commit

--- components/page-sections/landing-page/Common/Enter-email-and-get-started.tsx
+++ components/page-sections/landing-page/Common/Enter-email-and-get-started.tsx
@@ -1,25 +1,57 @@
 import en from '@/locales/page-sections/landing-page/@Section-Main/en'
 import hi from '@/locales/page-sections/landing-page/@Section-Main/hi'
 import { useRouter } from 'next/router'
+import { useLocalStorage } from 'react-use'
+import { useForm } from 'react-hook-form'
 
 export const Enter_Email_And_Get_Started: React.FC = () => {
   const router = useRouter()
   const { locale } = router
   const t = locale === 'en' ? en : hi
 
+  console.log(t)
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm()
+
+  const [userInputEmail, setUserInputEmail] = useLocalStorage(
+    'user-input-email',
+    ''
+  )
+
+  const onSubmit = (data: any) => {
+    setUserInputEmail(data.email)
+  }
+
   return (
     <div className="mt-5">
       <h3 className="sm:text-[18px] text-[16px]">{t.h3}</h3>
 
-      <form className="md:mt-5 mt-2 flex lg:flex-row flex-col justify-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="md:mt-5 mt-2 flex lg:flex-row flex-col justify-center"
+      >
         <label htmlFor="email">
           <input
+            {...register('email', { required: true })}
             type="email"
             name="email"
-            id=""
+            defaultValue={userInputEmail}
             placeholder={t.placeholder}
-            className="text-black focus:outline-none py-3 md:py-5 px-3 lg:rounded-l-sm lg:rounded-none rounded-sm md:w-[500px] w-full"
+            className={`relative text-black focus:outline-none py-3 md:py-5 px-3 lg:rounded-l-sm lg:rounded-none rounded-sm md:w-[500px] w-full ${
+              errors.email?.type === 'required' &&
+              'border-b-[2px] border-[#ff9f38]'
+            }`}
           />
+
+          {errors.email?.type === 'required' && (
+            <p className="text-[#ff9f38] text-base absolute mt-1 ml-1">
+              {locale === 'en' ? 'Email is required' : 'ईमेल डालना ज़रूरी है!'}
+            </p>
+          )}
         </label>
 
         <button className="lg:mt-0 mt-3 lg:m-0 m-auto bg-red hover:bg-[#f40612] lg:rounded-r-sm lg:rounded-none rounded-sm lg:px-7 px-[16px] py-[8px] lg:text-[30px] focus:outline-none flex items-center">
