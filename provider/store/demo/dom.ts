1f7c723ab5cea5d42a8085d538071e5774e8521f
shobhit-jain <shobhitjain.work@gmail.com>
2021-06-09 18:29:06 +0530

    commit

--- provider/store/demo/dom.ts
+++ provider/store/demo/dom.ts
@@ -1,11 +1,24 @@
 import { createContextStore, action } from 'easy-peasy'
 
 const Dom_Element_Store = createContextStore({
-  burger: undefined,
+  items: 0,
+  flowers: 0,
 
-  // Actions => updating the state from payload
-  setBurger: action((state: any, payload: any) => {
-    state.burger = payload
+  // Actions => increasing the value of items
+  increaseItem: action((state: { items: number }) => {
+    state.items += 1
+    return
+  }),
+
+  // Actions => decreasing the value of items
+  decreaseItem: action((state: { items: number }) => {
+    state.items <= 0 ? state.items : (state.items -= 1)
+    return
+  }),
+
+  // Actions => increasing the value of items
+  increaseFlower: action((state: { flowers: number }) => {
+    state.flowers += 1
     return
   }),
 })
