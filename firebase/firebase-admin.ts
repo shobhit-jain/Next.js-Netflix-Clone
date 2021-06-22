4118442126082aabede32547d74aca41fb4dbe67
shobhit-jain <shobhitjain.work@gmail.com>
2021-06-23 02:16:35 +0530

    commit

--- firebase/firebase-admin.ts
+++ firebase/firebase-admin.ts
@@ -4,7 +4,7 @@ if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      privateKey: process.env.private_key,
+      privateKey: process.env.private_key.replace(/\\n/g, '\n'),
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DATABASE_URL,
